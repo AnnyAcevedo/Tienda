ac1511a787742c2cf93253dae1a16b459be06f9d
Anny <annieacmg17@gmail.com>
2024-11-17 23:30:28 -0600

    Commit Semana 9

--- src/main/java/com/tienda/domain/Producto.java
+++ src/main/java/com/tienda/domain/Producto.java
@@ -15,7 +15,7 @@ public class Producto implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idProducto;
-    private Long idCategoria;
+   // private Long idCategoria;
     private String descripcion;
     private String detalle;
     private double precio;
@@ -23,7 +23,12 @@ public class Producto implements Serializable{
     
     private String rutaImagen;
     private boolean activo;
+    
+    @ManyToOne
+    @JoinColumn (name = "id_Categoria")
+    private Categoria categoria;
 }
+
 
 /*
 create table categoria (
