f2a2b6b31bc5ea8553c894947cd6acb3c870ac05
Anny <annieacmg17@gmail.com>
2024-12-17 13:02:57 -0600

    Semana 13
    Ultimo commit

--- src/main/java/com/tienda/ProjectConfig.java
+++ src/main/java/com/tienda/ProjectConfig.java
@@ -19,6 +19,8 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
 
@@ -100,6 +102,18 @@ public class ProjectConfig implements WebMvcConfigurer {
         builder.userDetailsService(userDetailsService)
                 .passwordEncoder(new BCryptPasswordEncoder());
 
+    }
+    
+    @Bean
+    public SpringResourceTemplateResolver resolver_0(){
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setOrder(0);
+        resolver.setCheckExistence(true);
+        
+        return resolver;
     }
 
 }
